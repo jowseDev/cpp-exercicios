98bbbfb376fc566a50723fcb9d7a8c4a92d17c65
José Augusto <jose2006martins@gmail.com>
2025-05-19 19:24:58 -0300

    atualizacoes-19-05-2025

---
+++ desafios/ds5/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 2. Faça um programa que leia a idade de uma pessoa e diga se ela é menor de idade, maior de idade, ou idosa (60 anos ou mais).
+    cout << "Digite sua idade: " << endl;
+    int i; cin >> i;
+    if(i > 60){
+    cout << "Você é velho!" << endl;
+    }else if(i < 18){
+    cout << "Voçê é menor de idade!!" << endl;
+    }else{
+    cout << "Voçê é adulto!" << endl;
+    }
+
+
+
+
+    return 0;
+}
